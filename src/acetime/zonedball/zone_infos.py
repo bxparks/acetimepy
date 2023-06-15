b1bb3ab23d59153c375d44f751d34fa32f569b55
Brian Park <brian@xparks.net>
2023-06-15 09:35:01 -0700

    zonedb*: add [start,until) years of accurate transitions

--- src/acetime/zonedball/zone_infos.py
+++ src/acetime/zonedball/zone_infos.py
@@ -9,7 +9,7 @@
 #     --scope complete
 #     --skip_bufestimator
 #     --start_year 1800
-#     --until_year 10000
+#     --until_year 2200
 #
 # using the TZ Database files
 #
@@ -28,9 +28,12 @@
 # Supported Zones: 596 (350 zones, 246 links)
 # Unsupported Zones: 0 (0 zones, 0 links)
 #
+# Requested Years: [1800,2200]
+# Accurate Years: [-32767,32767]
+#
 # Original Years:  [1844,2087]
 # Generated Years: [1844,2087]
-# Lower/Upper Truncated: [False, False]
+# Lower/Upper Truncated: [False,False]
 #
 # Records:
 #   Infos: 596
@@ -54,7 +57,9 @@ from .zone_policies import *
 
 TZDB_VERSION = '2023c'
 START_YEAR = 1800
-UNTIL_YEAR = 10000
+UNTIL_YEAR = 2200
+START_YEAR_ACCURATE = -32767
+UNTIL_YEAR_ACCURATE = 32767
 
 # ---------------------------------------------------------------------------
 # Supported zones: 350
